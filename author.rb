4632f725f504af876f84d53d44f9d9db8c820fc3
unknown <Riyanagwl@gmail.com>
2023-02-27 15:57:58 +0300

    add genre,author and book

---
+++ author.rb
@@ -0,0 +1,6 @@
+class Author
+  attr_accessor :first_name
+  def initialize(first_name)
+    @first_name = first_name
+  end
+end
---
+++ book.rb
@@ -0,0 +1,11 @@
+ class Book < Item
+  attr_accessor :publisher, :cover_state
+  def initialize(published_date, publisher, cover_state)
+    super(published_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+  def can_be_archived?
+    super || @cover_state == 'bad'
+  end
+end
