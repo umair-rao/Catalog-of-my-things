a9758038ba7bd7caf8405e08024afff393807bdb
shella12 <ayeshaarshad4567@gmail.com>
2023-02-28 17:29:54 +0500

    fix lnter errors

--- label.rb
+++ label.rb
@@ -1,9 +1,8 @@
 class Label
-	attr_accessor :title, :color
-  
-	def initialize(title, color)
-	  @title = title
-	  @color = color
-	end
+  attr_accessor :title, :color
+
+  def initialize(title, color)
+    @title = title
+    @color = color
   end
-  
+end
--- source.rb
+++ source.rb
@@ -1,14 +1,13 @@
 class Source
-	attr_accessor :name
-  
-	def initialize(name)
-	  @name = name
-	end
-  
-	def add_item(item)
-	  @items ||= []
-	  @items << item
-	  item.source = self
-	end
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
   end
-  
+
+  def add_item(item)
+    @items ||= []
+    @items << item
+    item.source = self
+  end
+end
