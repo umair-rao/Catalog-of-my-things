cd03de335f6416b7d44f4f758832c56897dc2495
shella12 <ayeshaarshad4567@gmail.com>
2023-02-28 13:17:14 +0500

    Fix linter errors

--- game.rb
+++ game.rb
@@ -7,15 +7,12 @@ class Game < Item
   def initialize(multiplayer, last_played_at, genere, label, publish_date)
     super(genere, label, publish_date)
     @multiplayer = multiplayer
-    @last_played_at = last_played_at  
-
+    @last_played_at = last_played_at
   end
 
   # private
 
   def can_be_archieved?
-    time = Time.new
     super && Date.parse(@last_played_at).year < Date.today.year - 10
-
   end
 end
