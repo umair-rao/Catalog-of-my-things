93968fc8635f400bdf653d6ca523fa709697ead7
umair-rao <umairmuqeem@gmail.com>
2023-02-28 13:19:48 +0500

    Add Label and Source

---
+++ label.rb
@@ -0,0 +1,9 @@
+class Label
+	attr_accessor :title, :color
+  
+	def initialize(title, color)
+	  @title = title
+	  @color = color
+	end
+  end
+  
---
+++ source.rb
@@ -0,0 +1,14 @@
+class Source
+	attr_accessor :name
+  
+	def initialize(name)
+	  @name = name
+	end
+  
+	def add_item(item)
+	  @items ||= []
+	  @items << item
+	  item.source = self
+	end
+  end
+  
