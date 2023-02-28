6e13f6cac8a00bb78a82b6c37c75293e2248c50a
shella12 <ayeshaarshad4567@gmail.com>
2023-02-28 17:33:37 +0500

    fix lnter errors

--- item.rb
+++ item.rb
@@ -1,17 +1,16 @@
 class Item
-	attr_accessor :published_date, :archived
-  
-	def initialize(published_date)
-	  @published_date = published_date
-	  @archived = false
-	end
-  
-	def can_be_archived?
-	  Date.today.year - @published_date.year > 10
-	end
-  
-	def move_to_archive
-	  @archived = true if can_be_archived?
-	end
+  attr_accessor :published_date, :archived
+
+  def initialize(published_date)
+    @published_date = published_date
+    @archived = false
   end
-  
+
+  def can_be_archived?
+    Date.today.year - @published_date.year > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+end
