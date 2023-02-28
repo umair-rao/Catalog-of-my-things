b3ff356f1bc496adb714be7195224a457c129271
unknown <Riyanagwl@gmail.com>
2023-02-28 18:18:23 +0300

    add entry file

---
+++ app.rb
@@ -0,0 +1,78 @@
+class LibraryItem
+  attr_accessor :title, :author, :label, :source, :genre
+
+  def initialize(title, author, label, source, genre)
+    @title = title
+    @author = author
+    @label = label
+    @source = source
+    @genre = genre
+  end
+end
+
+class Book < LibraryItem
+end
+
+class MusicAlbum < LibraryItem
+end
+
+class Movie < LibraryItem
+end
+
+class Game < LibraryItem
+end
+
+class Library
+  attr_accessor :items
+
+  def initialize
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
+
+  def list_all_items(type)
+    puts "Listing all #{type}:"
+    @items.each do |item|
+      puts "- #{item.title}" if item.class.to_s == type
+    end
+  end
+
+  def list_all_genres
+    puts 'Listing all genres:'
+    genres = []
+    @items.each do |item|
+      genres << item.genre unless genres.include?(item.genre)
+    end
+    genres.each { |genre| puts "- #{genre}" }
+  end
+
+  def list_all_labels
+    puts 'Listing all labels:'
+    labels = []
+    @items.each do |item|
+      labels << item.label unless labels.include?(item.label)
+    end
+    labels.each { |label| puts "- #{label}" }
+  end
+
+  def list_all_authors
+    puts 'Listing all authors:'
+    authors = []
+    @items.each do |item|
+      authors << item.author unless authors.include?(item.author)
+    end
+    authors.each { |author| puts "- #{author}" }
+  end
+
+  def list_all_sources
+    puts 'Listing all sources:'
+    sources = []
+    @items.each do |item|
+      sources << item.source unless sources.include?(item.source)
+    end
+    sources.each { |source| puts "- #{source}" }
+  end
+end
--- book.rb
+++ book.rb
@@ -1,13 +1,24 @@
+require_relative 'item'
+
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(published_date, publisher, cover_state)
-    super(published_date)
+  def initialize(publisher, cover_state, publish_date)
+    super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end
 
   def can_be_archived?
     super || @cover_state == 'bad'
+  end
+
+  def to_json(*_args)
+    {
+      publisher: @publisher,
+      cover_state: @cover_state,
+      publish_date: @publish_date,
+      label: label
+    }
   end
 end
