2dac72fe5e9bb08bd4bd0935f392e8b6d6b53d14
shella12 <ayeshaarshad4567@gmail.com>
2023-03-01 14:40:00 +0500

    Refactor main.rb

--- book.rb
+++ book.rb
@@ -1,16 +1,17 @@
-require_relative 'item'
-
+require_relative './item'
 class Book < Item
-  attr_accessor :publisher, :cover_state
+  attr_accessor :publisher, :cover_state, :publish_date, :author, :title
 
-  def initialize(publisher, cover_state, publish_date)
+  def initialize(publisher, cover_state, publish_date, author, title)
     super(publish_date)
+    @author = author
+    @title = title
     @publisher = publisher
     @cover_state = cover_state
   end
 
   def can_be_archived?
-    super || @cover_state == 'bad'
+    super() && @cover_state == 'bad' && @publish_date <= Date.today - (10 * 365)
   end
 
   def to_json(*_args)
